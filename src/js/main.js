6b9fd2a1625407c72c9a7407cf7e8e7676967e6d
sasinskil <sasinskil@gmail.com>
2019-08-26 21:45:07 +0200

    added a method to fetch repositories

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,42 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
+console.log(`Hello friend!`);
+
+const projectsList = document.querySelector('.projects__list--js');
+
+
+const findRepositories = () => {
+
+  fetch(`https://api.github.com/users/sasinskil/repos?sort=created&direction=desc`)
+    .then(resp => resp.json())
+    .then(resp => {
+      const repos = resp;
+
+      if (repos.length > 0) {
+        for (let repo of repos) {
+          const { name, description, homepage, html_url } = repo;
+
+          if (homepage) {
+            projectsList.innerHTML += `
+            <li class="projects__item">
+              <div class="projects__content">
+               <img class="projects__github-logo" src="assets/img/github-icon.png" alt="">
+               <h4 class="projects__sub-title">${name}</h4>
+               <p class="projects__description">${description}</p>
+              </div>
+            <footer class="projects__footer">
+              <a class="projects__link projects__link--demo" href="${homepage}">Demo</a>
+              <a class="projects__link projects__link--github" href="${html_url}">Github</a>
+            </footer>
+          </li>
+            `
+          }
+        }
+      }
+    })
+    .catch(err => alert(`Something went wrong, ${err}`));
 }
 
-// place your code below
 
-
-console.log(`Hello world!`);
-
-
+findRepositories();
