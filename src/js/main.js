e41a55d7490e5b679449e228a3e98cbc9ddd44a4
sasinskil <sasinskil@gmail.com>
2019-09-08 13:00:24 +0200

    og corrections, improvement of projects display logic

--- src/js/main.js
+++ src/js/main.js
@@ -8,19 +8,17 @@ const projectsList = document.querySelector('.projects__list--js');
 
 if (projectsList) {
 
-  const findRepositories = () => {
+  fetch(`https://api.github.com/users/sasinskil/repos?sort=created&direction=desc`)
+    .then(resp => resp.json())
+    .then(resp => {
+      const repos = resp;
 
-    fetch(`https://api.github.com/users/sasinskil/repos?sort=created&direction=desc`)
-      .then(resp => resp.json())
-      .then(resp => {
-        const repos = resp;
+      if (repos.length > 0) {
+        for (let repo of repos) {
+          const { name, description, homepage, html_url } = repo;
 
-        if (repos.length > 0) {
-          for (let repo of repos) {
-            const { name, description, homepage, html_url } = repo;
-
-            if (homepage) {
-              projectsList.innerHTML += `
+          if (homepage) {
+            projectsList.innerHTML += `
             <li class="projects__item">
               <div class="projects__content">
                <img class="projects__github-logo" src="assets/img/github-icon.svg" alt="Github logo">
@@ -28,17 +26,14 @@ if (projectsList) {
                <p class="projects__description">${description}</p>
               </div>
             <div class="projects__footer">
-              <a class="projects__link projects__link--demo" href="${homepage}">Demo</a>
-              <a class="projects__link projects__link--github" href="${html_url}">Github</a>
+              <a class="projects__link projects__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer">Demo</a>
+              <a class="projects__link projects__link--github" href="${html_url}" target="_blank" rel="nofollow noreferrer">Github</a>
             </div>
           </li>
             `
-            }
           }
         }
-      })
-      .catch(err => alert(`Something went wrong, ${err}`));
-  }
-
-  findRepositories();
+      }
+    })
+    .catch(err => alert(`Something went wrong, ${err}`));
 }
