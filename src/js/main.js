517b15d5dcf3cde81a553ad09c11d489a4977862
sasinskil <sasinskil@gmail.com>
2021-01-30 19:23:56 +0100

    refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,66 +1,76 @@
 "use strict";
 
+let projectsList,
+  moreButton,
+  githubReposAPiUrl,
+  createProjectItem,
+  addToProjectList,
+  manageElements,
+  fetchRepos,
+  repos;
 
-console.log(`Hello friend!`);
+projectsList = document.querySelector(".projects__list--js");
+moreButton = document.querySelector(".projects__more--js");
+githubReposAPiUrl = `https://api.github.com/users/sasinskil/repos`;
 
-const projectsList = document.querySelector('.projects__list--js');
-const moreButton = document.querySelector('.projects__more--js');
-
-
-if (projectsList) {
-
-  moreButton.classList.add('projects__more--hide');
-  fetch(`https://api.github.com/users/sasinskil/repos?sort=updated&direction=desc`)
-    .then(resp => resp.json())
-    .then(resp => {
-      moreButton.classList.remove('projects__more--hide');
-      const repos = resp;
-
-      if (repos.length > 0) {
-        for (let repo of repos.slice(0, 6)) {
-          const { name, description, homepage, html_url } = repo;
-
-          if (projectsList && homepage) {
-            projectsList.innerHTML +=
-            `
-            <li class="projects__item">
-              <div class="projects__content">
-               <img class="projects__github-logo" src="assets/img/github-icon.svg" alt="Github logo">
-               <h4 class="projects__sub-title">${name}</h4>
-               <p class="projects__description">${description}</p>
-              </div>
-            <div class="projects__footer">
-              ${homepage ? `<a class="projects__link projects__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer">Demo</a>` : ''}
-              <a class="projects__link projects__link--github" href="${html_url}" target="_blank" rel="nofollow noreferrer">Github</a>
-            </div>
-          </li>`
+createProjectItem = ({ name, description, homepage, html_url }) => {
+  return `
+    <li class="projects__item">
+        <div class="projects__content">
+          <img class="projects__github-logo" src="assets/img/github-icon.svg" alt="Github logo">
+          <h4 class="projects__sub-title">${name}</h4>
+          <p class="projects__description">${description}</p>
+        </div>
+        <div class="projects__footer">
+          ${
+            homepage
+              ? `<a class="projects__link projects__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer">Demo</a>`
+              : ""
           }
+          <a class="projects__link projects__link--github" href="${html_url}" target="_blank" rel="nofollow noreferrer">Github</a>
+        </div>
+      </li>
+    `;
+}
+
+addToProjectList = (repo) => {
+  const newElement = createProjectItem(repo);
+  projectsList.innerHTML += newElement;
+}
+
+manageElements = () => {
+  if (moreButton.innerHTML === "Less") {
+    projectsList.innerHTML = "";
+    for (let repo of repos.slice(0, 4)) {
+      addToProjectList(repo);
+    }
+    moreButton.innerHTML = "More";
+  } else {
+    for (let repo of repos.slice(4)) {
+      addToProjectList(repo);
+    }
+    moreButton.innerHTML = "Less";
+  }
+}
+
+fetchRepos = (apiUrl) => {
+  fetch(apiUrl)
+    .then((resp) => resp.json())
+    .then((data) => {
+      repos = data;
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+      if (projectsList) {
+        if (repos.length > 0) {
+          for (let repo of repos.slice(0, 4)) {
+            addToProjectList(repo);
+          }
+
+          moreButton.addEventListener("click", manageElements);
         }
-
-        moreButton.addEventListener('click', () => {
-          for (let repo of repos.slice(6)) {
-            const { name, description, homepage, html_url } = repo;
-
-            if (homepage) {
-              projectsList.innerHTML += 
-              `
-              <li class="projects__item">
-                <div class="projects__content">
-                 <img class="projects__github-logo" src="assets/img/github-icon.svg" alt="Github logo">
-                 <h4 class="projects__sub-title">${name}</h4>
-                 <p class="projects__description">${description}</p>
-                </div>
-              <div class="projects__footer">
-                ${homepage ? `<a class="projects__link projects__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer">Demo</a>` : ''}
-                <a class="projects__link projects__link--github" href="${html_url}" target="_blank" rel="nofollow noreferrer">Github</a>
-              </div>
-            </li>`
-            }
-          }
-
-          moreButton.classList.add('projects__more--hide');
-        });
       }
     })
-    .catch(err => alert(`Something went wrong, ${err}`));
+    .catch((err) => console.log(`Something went wrong, ${err}`));
 }
+
+fetchRepos(githubReposAPiUrl);
